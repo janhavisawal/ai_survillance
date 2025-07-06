01b055c53d5a20afce5bc44795fa9043457ca89e
janhavisawal <53596988+janhavisawal@users.noreply.github.com>
2025-07-06 12:28:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -27,6 +27,22 @@ const nextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
           },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=*, microphone=*, geolocation=()',
+          },
         ],
       },
     ];
